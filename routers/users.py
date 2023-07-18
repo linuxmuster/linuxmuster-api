3812ede3259188f01f71cab15f4240f4c432209b
kiarn <arnaud@kientz.me>
2023-07-18 12:49:06 +0200

    Example to get pw as pdf.

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi.responses import FileResponse
 import subprocess
 
 from security import PermissionChecker
@@ -49,3 +50,20 @@ def test_root_perm(user: str, auth: bool = Depends(PermissionChecker(["student"]
                             )"""
 
     return lc.get_single(LMNUser, ldap_filter)
+
+@router.get("/password/{user}/{schoolclass}")
+def print_class_password(user: str, schoolclass: str, auth: bool = Depends(PermissionChecker(["teacher"]))):
+    cmd = [
+        'sophomorix-print', '--school', 'default-school',
+        '--caller', user,
+        '--template', '/usr/share/sophomorix/lang/latex/templates/datalist-DE-36-template.tex',
+        '--class', schoolclass, '-jj'
+    ]
+
+    shell_env = {'TERM': 'xterm', 'SHELL': '/bin/bash',  'PATH': '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',  'HOME': '/root', '_': '/usr/bin/python3'}
+    subprocess.check_call(cmd, shell=False, env=shell_env)
+
+    filename = f'{schoolclass}-{user}.pdf'
+    file_path = f'/var/lib/sophomorix/print-data/{filename}'
+
+    return FileResponse(path=file_path, filename=filename, media_type='application/pdf')
