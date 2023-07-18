4d2ec07ca41da57543752d7da8468ef4c65ee318
kiarn <arnaud@kientz.me>
2023-07-18 10:13:31 +0200

    Add endpoint to test root permissions.

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
+import subprocess
 
 from security import PermissionChecker
 from utils import lmn_getSophomorixValue
@@ -18,3 +19,9 @@ def get_all_users(auth: bool = Depends(PermissionChecker("globaladministrator"))
 def get_user_details(user: str, auth: bool = Depends(PermissionChecker(["schooladministrator", "globaladministrator"]))):
     cmd = f"sophomorix-user -ivvv -u {user} -jj".split()
     return lmn_getSophomorixValue(cmd, '')
+
+@router.post("/testrootperm")
+def test_root_perm(auth: bool = Depends(PermissionChecker(["student"]))):
+    cmd = f"touch /root/testapi.log".split()
+    subprocess.check_call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    return "ok"
