a2b10fe9e792ea88db1ecf04192178715f4dcb3e
kiarn <arnaud@kientz.me>
2024-01-18 13:23:43 +0100

    Passwords of a schoolclass.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/schoolclasses.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/schoolclasses.py
@@ -25,24 +25,15 @@ def get_all_schoolclasses(auth: bool = Depends(PermissionChecker("globaladminist
 @router.get("/{schoolclass}")
 def get_schoolclass(schoolclass: str, auth: bool = Depends(PermissionChecker(["globaladministrator"]))):
     """
-    Get all details from a specific user.
+    Get all details from a specific schoolclass.
     """
 
-    return lr.get(f'/{schoolclass}')
+    return lr.get(f'/schoolclasses/{schoolclass}')
 
-@router.get("/password/{user}/{schoolclass}")
-def print_class_password(user: str, schoolclass: str, auth: bool = Depends(PermissionChecker(["teacher"]))):
-    cmd = [
-        'sophomorix-print', '--school', 'default-school',
-        '--caller', user,
-        '--template', '/usr/share/sophomorix/lang/latex/templates/datalist-DE-36-template.tex',
-        '--class', schoolclass, '-jj'
-    ]
+@router.get("/{schoolclass}/first_passwords")
+def get_schoolclass_passwords(schoolclass: str, auth: bool = Depends(PermissionChecker(["globaladministrator"]))):
+    """
+    Get all passwords from a specific schoolclass.
+    """
 
-    shell_env = {'TERM': 'xterm', 'SHELL': '/bin/bash',  'PATH': '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',  'HOME': '/root', '_': '/usr/bin/python3'}
-    subprocess.check_call(cmd, shell=False, env=shell_env)
-
-    filename = f'{schoolclass}-{user}.pdf'
-    file_path = f'/var/lib/sophomorix/print-data/{filename}'
-
-    return FileResponse(path=file_path, filename=filename, media_type='application/pdf')
+    return lr.get(f'/schoolclasses/{schoolclass}', dict=False).get_first_passwords()
