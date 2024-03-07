4ffa73130f91c3b11fdaafc384899337bdff11ee
kiarn <arnaud@kientz.me>
2024-03-07 10:03:07 +0100

    Check roles permissions.

--- usr/lib/python3/dist-packages/linuxmusterApi/security.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/security.py
@@ -85,10 +85,55 @@ class PermissionChecker:
         elif isinstance(roles, str):
             self.roles = [roles]
 
-    def __call__(self, who: dict = Depends(check_authentication_header), user=None) -> bool:
-        if who["role"] in self.roles:
+    def _check_role_permissions(self, who, requested_user):
+
+            role_rank = {
+                'globaladministrator': 4,
+                'schooladministrator': 3,
+                'teacher': 2,
+                'student': 1,
+                'examuser': 1,
+            }
+
+            identity = who['user']
+            identity_role = who['role']
+
+            # Global admins have all rights
+            if identity_role == 'globaladministrator':
+                return True
+
+            if requested_user.endswith('-exam'):
+                user_role = "examuser"
+            else:
+                user_role = lr.getval(f'/users/{requested_user}', 'sophomorixRole')
+
+            ## Some additional security checks
+            # Access forbidden for students
+            if identity_role not in ['teacher', 'schooladministrator']:
+                return False
+
+            # Same role but other user -> access forbidden
+            if identity_role == user_role:
+                # User can only change its own password, not from someone else with the same role
+                if identity != requested_user:
+                    return False
+            else:
+                # Check if the role rank of the user is greater than the user logged in
+                # If the role of the user can not be found, access forbidden
+                if role_rank.get(user_role, 5) > role_rank[identity_role]:
+                    return False
+
             return True
+
+    def __call__(self, who: dict = Depends(check_authentication_header), user=None) -> bool:
+
+        if who["role"] in self.roles or who["role"] == 'globaladministrator':
+            return True
+
         if who["user"] == user:
+            return True
+
+        if self._check_role_permissions(who, user):
             return True
 
         raise HTTPException(
