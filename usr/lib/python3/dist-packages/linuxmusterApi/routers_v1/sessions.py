3078923d05647bdea130581d2f570005be641103
kiarn <arnaud@kientz.me>
2024-04-17 08:51:37 +0200

    Move to ldapreader.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/sessions.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/sessions.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from security import RoleChecker
 from utils import lmn_getSophomorixValue
+from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 
 
 router = APIRouter(
@@ -13,22 +14,39 @@ router = APIRouter(
 # TODO: rewrite the whole with lr and lw
 # TODO: Permissions
 
-@router.get("/per_supervisor/{supervisor}")
+@router.get("/{supervisor}")
 def session_supervisor(supervisor: str, auth: bool = Depends(RoleChecker("GS"))):
-    cmd = f"sophomorix-session -i --supervisor {supervisor} -jj".split()
+    sessions = lr.get(f'/users/{supervisor}', dict=False).lmnsessions
+    sessionsList = []
+    for session in sessions:
+        s = {
+            'sid': session.sid,
+            'name': session.name,
+            'membersCount': session.membersCount,
+            'members': session.members,
+        }
+        sessionsList.append(s)
+    return sessionsList
+
+@router.get("/{supervisor}/{sessionsid}")
+def get_session_sessionname(supervisor:str, sessionsid: str, auth: bool = Depends(RoleChecker("GS"))):
+    sessions = lr.get(f'/users/{supervisor}', dict=False).lmnsessions
+    for session in sessions:
+        if sessionsid == session.sid:
+            return {
+                'sid': session.sid,
+                'name': session.name,
+                'membersCount': session.membersCount,
+                'members': session.members,
+            }
+    raise HTTPException(status_code=404, detail=f"Session {sessionsid} not found by {supervisor}")
+
+@router.delete("/{supervisor}/{sessionsid}")
+def delete_session(sessionsid: str, auth: bool = Depends(RoleChecker("GS"))):
+    cmd = f"sophomorix-session --session {sessionsid} --kill -jj".split()
     return lmn_getSophomorixValue(cmd, '')
 
-@router.get("/{session}")
-def get_session_sessionname(session: str, auth: bool = Depends(RoleChecker("GS"))):
-    cmd = f"sophomorix-session -i --session {session} -jj".split()
-    return lmn_getSophomorixValue(cmd, '')
-
-@router.delete("/{session}")
-def delete_session(session: str, auth: bool = Depends(RoleChecker("GS"))):
-    cmd = f"sophomorix-session --session {session} --kill -jj".split()
-    return lmn_getSophomorixValue(cmd, '')
-
-@router.post("/session/{supervisor}/{sessionname}")
+@router.post("/{supervisor}/{sessionname}")
 def session_create(supervisor: str, sessionname: str, auth: bool = Depends(RoleChecker("GS"))):
     cmd = f"sophomorix-session --create --supervisor {supervisor} --comment {sessionname} -jj".split()
     return lmn_getSophomorixValue(cmd, '')
