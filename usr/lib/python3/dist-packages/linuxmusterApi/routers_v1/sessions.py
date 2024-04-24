1478f4e976337d2c8d6fb7173aabe4e09fc55907
kiarn <arnaud@kientz.me>
2024-04-24 16:15:04 +0200

    Delete session.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/sessions.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/sessions.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 
 from security import RoleChecker
 from utils import lmn_getSophomorixValue
-from linuxmusterTools.ldapconnector import LMNLdapReader as lr
+from linuxmusterTools.ldapconnector import LMNLdapReader as lr, LMNLdapWriter as lw
 
 
 router = APIRouter(
@@ -41,10 +41,23 @@ def get_session_sessionname(supervisor:str, sessionsid: str, auth: bool = Depend
             }
     raise HTTPException(status_code=404, detail=f"Session {sessionsid} not found by {supervisor}")
 
-@router.delete("/{supervisor}/{sessionsid}")
-def delete_session(sessionsid: str, auth: bool = Depends(RoleChecker("GS"))):
-    cmd = f"sophomorix-session --session {sessionsid} --kill -jj".split()
-    return lmn_getSophomorixValue(cmd, '')
+@router.delete("/{supervisor}/{sessionsid}", status_code=204)
+def delete_session(supervisor:str, sessionsid: str, response: Response, auth: bool = Depends(RoleChecker("GS"))):
+    sessions = lr.get(f'/users/{supervisor}', dict=False).lmnsessions
+    modified = False
+    for index, session in enumerate(sessions):
+        if sessionsid == session.sid:
+            sessions.pop(index)
+            modified = True
+            break
+
+    if modified:
+        ldap_sessions = [f"{session.sid};{session.name};{','.join(session.members)};" for session in sessions]
+        lw.set(supervisor, 'user', {'sophomorixSessions': ldap_sessions})
+    else:
+        response.status_code = status.HTTP_304_NOT_MODIFIED
+
+    return
 
 @router.post("/{supervisor}/{sessionname}")
 def session_create(supervisor: str, sessionname: str, auth: bool = Depends(RoleChecker("GS"))):
