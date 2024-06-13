e5c8289559faf745115a03842df262548e6ebbc2
kiarn <arnaud@kientz.me>
2024-06-13 12:19:21 +0200

    Stop exam.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/exam.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/exam.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from pydantic import BaseModel, Field
+from time import localtime, strftime
 
 from security import UserListChecker, AuthenticatedUser
 from sophomorix import lmn_getSophomorixValue
@@ -14,6 +15,11 @@ router = APIRouter(
 
 class UserList(BaseModel):
     users: list | None = None
+
+class StopExam(BaseModel):
+    users: list | None = None
+    group_type: str | None = None
+    group_name: str | None = None
 
 @router.post("/start")
 def start_exam_mode(userlist: UserList, who: AuthenticatedUser = Depends(UserListChecker("GST"))):
@@ -39,3 +45,28 @@ def start_exam_mode(userlist: UserList, who: AuthenticatedUser = Depends(UserLis
         raise HTTPException(status_code=500, detail=f"Error starting exam mode: {str(e)}")
 
 
+@router.post("/stop")
+def stop_exam_mode(stopexam: StopExam, who: AuthenticatedUser = Depends(UserListChecker("GST"))):
+    """
+    Accessible by global-administrators, school-administrators and teachers.
+    Use sophomorix.
+    """
+
+    if not stopexam.users:
+        # Nothing to do
+        return
+
+    now = strftime("%Y-%m-%d_%Hh%Mm%S", localtime())
+    target = f'EXAM_{stopexam.group_type}_{stopexam.group_name}_{now}'
+
+    try:
+        sophomorixCommand = [
+            'sophomorix-exam-mode',
+            '--unset',
+            '--subdir', f'transfer/collected/{target}',
+            '-j',
+            '--participants', ','.join(stopexam.users)
+        ]
+        lmn_getSophomorixValue(sophomorixCommand, 'COMMENT_EN')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error stoping exam mode: {str(e)}")
