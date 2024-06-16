f27230eb864f836bebf15fe92f65356bd21e815a
kiarn <arnaud@kientz.me>
2024-06-16 17:35:29 +0200

    Update documentation.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/teachers.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/teachers.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi.responses import FileResponse
-import subprocess
 
-from security import RoleChecker
+from security import RoleChecker, AuthenticatedUser
 from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 
 
@@ -18,20 +16,41 @@ def check_teacher(teacher):
     if teacher not in teachers:
         raise HTTPException(status_code=404, detail=f"Teacher {teacher} not found")
 
-@router.get("/")
-def get_all_teachers(auth: bool = Depends(RoleChecker("GS"))):
+@router.get("/", name='List all teachers')
+def get_all_teachers(who: AuthenticatedUser = Depends(RoleChecker("GS"))):
     """
-    Get basic informations from all teachers.
-    Accessible by global-administrators and school-administrators.
+    ## Get all informations from all teachers.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    \f
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all teachers details (dict)
+    :rtype: list
     """
+
 
     return lr.get('/roles/teacher')
 
-@router.get("/{teacher}")
-def get_teacher(teacher: str, auth: bool = Depends(RoleChecker("GS"))):
+@router.get("/{teacher}", name="Get informations of a specific teacher")
+def get_teacher(teacher: str, who: AuthenticatedUser = Depends(RoleChecker("GS"))):
     """
-    Get all details from a specific teacher.
-    Accessible by global-administrators and school-administrators.
+    ## Get all informations of a specific teacher.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    \f
+    :param teacher: samaccountname of the requested teacher
+    :type teacher: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all teachers details (dict)
+    :rtype: list
     """
 
     check_teacher(teacher)
