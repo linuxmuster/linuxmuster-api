44a582f3f1a6dc14d6413274bee2302047507aaa
kiarn <arnaud@kientz.me>
2024-06-16 17:27:45 +0200

    Update documentation.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/users.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from pydantic import BaseModel, Field
 
-from security import RoleChecker, UserChecker
+from security import RoleChecker, UserChecker, AuthenticatedUser
 from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 from linuxmusterTools.ldapconnector import LMNLdapWriter as lw
 from linuxmusterTools.samba_util import UserManager
@@ -24,30 +24,73 @@ class SetCurrentPassword(BaseModel):
     password: str
     set_first: bool = Field(default= False)
 
-@router.get("/")
-def get_all_users(auth: bool = Depends(RoleChecker("G"))):
+@router.get("/", name="List all users")
+def get_all_users(who: AuthenticatedUser = Depends(RoleChecker("G"))):
     """
-    Get basic informations from all users.
-    Accessible by global-administrators.
+    ## Get basic informations from all users.
+
+    Output informations are sn, givenName, sophomorixRole, sophomorixAdminClass.
+
+    ### Access
+    - global-administrators
+
+    \f
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all users details (dict)
+    :rtype: list
     """
+
 
     return lr.get('/users', attributes=['sn', 'givenName', 'sophomorixRole', 'sophomorixAdminClass'])
 
-@router.get("/{user}")
-def get_user(user: str, auth: bool = Depends(UserChecker("GST"))):
+@router.get("/{user}", name="User details")
+def get_user(user: str, who: AuthenticatedUser = Depends(UserChecker("GST"))):
     """
-    Get all details from a specific user.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## Get all informations of a specific user.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers (own data and students)
+
+    \f
+    :param user: The user to get the details from (samaccountname)
+    :type user: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: All user's details
+    :rtype: dict
     """
+
 
     return lr.get(f'/users/{user}')
 
-@router.post("/{user}/set-first-password")
-def set_first_user_password(user: str, password: SetFirstPassword, auth: bool = Depends(UserChecker("GST"))):
+@router.post("/{user}/set-first-password", name="Set user's first password")
+def set_first_user_password(user: str, password: SetFirstPassword, who: AuthenticatedUser = Depends(UserChecker("GST"))):
     """
-    Set first password from a specific user.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## Set the first password of the user.
+
+    The **first password**, also known as default password, is the readable password
+    in the LDAP account, and the one to which it's possible to retrograde if the
+    user looses its **current password**.
+    If the flag *set_current* is set to true, the current password will be updated
+    too.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers (own data and students)
+
+    \f
+    :param user: The user to get the details from (samaccountname)
+    :type user: basestring
+    :param password: The password o set
+    :type password: SetFirstPassword
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
     """
+
 
     # TODO : paswword constraints ?
     lw.set(user, 'user', {'sophomorixFirstPassword': password.password})
@@ -57,12 +100,37 @@ def set_first_user_password(user: str, password: SetFirstPassword, auth: bool = 
         except Exception as e:
             raise HTTPException(status_code=400, detail=f"Cannot set current password: {str(e)}")
 
-@router.post("/{user}/set-current-password")
-def set_current_user_password(user: str, password: SetCurrentPassword, auth: bool = Depends(UserChecker("GST"))):
+@router.post("/{user}/set-current-password", name="Set user's current password")
+def set_current_user_password(user: str, password: SetCurrentPassword, who: AuthenticatedUser = Depends(UserChecker("GST"))):
     """
-    Set current password from a specific user.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## Set the current password of the user.
+
+    The **current password** is the password actually used by the user, not
+    readable in the LDAP tree.
+
+    The **first password**, also known as default password, is the readable password
+    in the LDAP account, and the one to which it's possible to retrograde if the
+    user looses its **current password**.
+    If the flag *set_first* is set to true, the first password will be updated
+    too.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers (own data and students)
+
+    ### TODO
+    - Password constraints ?
+
+    \f
+    :param user: The user to get the details from (samaccountname)
+    :type user: basestring
+    :param password: The password o set
+    :type password: SetCurrentPassword
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
     """
+
 
     try:
         user_manager.set_password(user, password.password)
@@ -70,16 +138,30 @@ def set_current_user_password(user: str, password: SetCurrentPassword, auth: boo
         raise HTTPException(status_code=400, detail=str(e))
 
     if password.set_first:
-        # TODO : paswword constraints ?
         lw.set(user, 'user', {'sophomorixFirstPassword': password.password})
 
 
-@router.get("/{user}/quotas")
-def get_user_quotas(user: str, auth: bool = Depends(UserChecker("GST"))):
+@router.get("/{user}/quotas", name='Get the quotas of a specific user')
+def get_user_quotas(user: str, who: AuthenticatedUser = Depends(UserChecker("GST"))):
     """
-    Get current used quotas from a specific user.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## Get the actual quotas of a specific user.
+
+    Given informations per share are: used quota, soft limit and hard limit.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers (own data and students)
+
+    \f
+    :param user: samaccountname of the user to check
+    :type user: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: All available per share quota informations
+    :rtype: dict
     """
+
 
     try:
        return linuxmusterTools.quotas.get_user_quotas(user)
