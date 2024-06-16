c194f4912e70b584a1d13895cfc02ed8cbf1fb38
kiarn <arnaud@kientz.me>
2024-06-16 20:38:16 +0200

    Update documentation.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/schoolclasses.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/schoolclasses.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 
-from security import RoleChecker
+from security import RoleChecker, AuthenticatedUser
 from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 
 
@@ -15,45 +15,106 @@ def check_schoolclass(schoolclass):
     if schoolclass not in schoolclasses:
         raise HTTPException(status_code=404, detail=f"Schoolclass {schoolclass} not found")
 
-@router.get("/")
-def get_all_schoolclasses(auth: bool = Depends(RoleChecker("GST"))):
+@router.get("/", name="List all schoolclasses")
+def get_all_schoolclasses(who: AuthenticatedUser = Depends(RoleChecker("GST"))):
     """
-    Get alls schoolclasses.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## List all schoolclasses with all available informations.
+
+    Output informations are e.g. cn, dn, members, etc...
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers
+
+    \f
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all schoolclasses details (dict)
+    :rtype: list
     """
+
 
     return lr.get('/schoolclasses')
 
-@router.get("/{schoolclass}")
-def get_schoolclass(schoolclass: str, auth: bool = Depends(RoleChecker("GST"))):
+@router.get("/{schoolclass}", name="Get details of a specific schoolclass")
+def get_schoolclass(schoolclass: str, who: AuthenticatedUser = Depends(RoleChecker("GST"))):
     """
-    Get all details from a specific schoolclass.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## List all available informations of a specific schooclass.
+
+    Output informations are e.g. cn, dn, members, etc...
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers
+
+    \f
+    :param schoolclass: cn of the requested schoolclass
+    :type schoolclass: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all schoolclasses details (dict)
+    :rtype: list
     """
+
 
     # TODO: Check group membership
     check_schoolclass(schoolclass)
 
     return lr.get(f'/schoolclasses/{schoolclass}')
 
-@router.get("/{schoolclass}/first_passwords")
-def get_schoolclass_passwords(schoolclass: str, auth: bool = Depends(RoleChecker("GST"))):
+@router.get("/{schoolclass}/first_passwords", name="Get all first passwords of the members of a specific schoolclass")
+def get_schoolclass_passwords(schoolclass: str, who: AuthenticatedUser = Depends(RoleChecker("GST"))):
     """
-    Get all passwords from a specific schoolclass.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## Get the first passwords of all members of a specific schooclass.
+
+    The **first password**, also known as default password, is the readable password
+    in the LDAP account, and the one to which it's possible to retrograde if the
+    user looses its **current password**.
+    The boolean *firstPasswordStillSet* indicates if the first password is still
+    used as current password.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers
+
+    \f
+    :param schoolclass: cn of the requested schoolclass
+    :type schoolclass: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all members and passwords details as dict (value, and boolean
+    to indicate if still set as current password
+    :rtype: list
     """
+
 
     # TODO: Check group membership
     check_schoolclass(schoolclass)
 
     return lr.get(f'/schoolclasses/{schoolclass}', dict=False).get_first_passwords()
 
-@router.get("/{schoolclass}/students")
-def get_schoolclass_passwords(schoolclass: str, auth: bool = Depends(RoleChecker("GST"))):
+@router.get("/{schoolclass}/students", name="Details of students of a specific schoolclass")
+def get_schoolclass_passwords(schoolclass: str, who: AuthenticatedUser = Depends(RoleChecker("GST"))):
     """
-    Get all students details from a specific schoolclass.
-    Accessible by global-administrators, school-administrators and teachers.
+    ## Get all details of all members of a specific schooclass.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+    - teachers
+
+    \f
+    :param schoolclass: cn of the requested schoolclass
+    :type schoolclass: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all members details (dict)
+    :rtype: list
     """
+
 
     # TODO: Check group membership
     check_schoolclass(schoolclass)
