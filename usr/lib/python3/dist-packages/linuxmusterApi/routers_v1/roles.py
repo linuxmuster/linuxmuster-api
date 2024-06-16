d7d59c6a1e322318b498a2ced8ebc268e8fbb58b
kiarn <arnaud@kientz.me>
2024-06-16 17:47:57 +0200

    Update documentation.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/roles.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/roles.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 
-from security import RoleChecker
+from security import RoleChecker, AuthenticatedUser
 from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 
 
@@ -10,22 +10,47 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/")
-def get_all_roles(auth: bool = Depends(RoleChecker("GS"))):
+@router.get("/", name="List all existing roles")
+def get_all_roles(who: AuthenticatedUser = Depends(RoleChecker("GS"))):
     """
-    Get all used roles.
-    Accessible by global-administrators and school-administrators.
+    ## List all existing roles
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    \f
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all roles
+    :rtype: list
     """
+
 
     return set([k['sophomorixRole'] for k in lr.get('/search/', attributes=['sophomorixRole']) if k['sophomorixRole']])
 
-@router.get("/{role}")
-def get_role_users(role: str, school: str | None = 'default-school', auth: bool = Depends(RoleChecker(["GS"]))):
+@router.get("/{role}", name="List all members with a specific role")
+def get_role_users(role: str, school: str | None = 'default-school', who: AuthenticatedUser = Depends(RoleChecker(["GS"]))):
     """
-    Get all users having a specific role. A valid role could be teacher, student, globaladministrator,
-    schooladministrator, etc ...
-    Accessible by global-administrators and school-administrators.
+    ## List all users (and all their details) having a specific role
+
+    The given role can be teacher, student, globaladministrator, etc ...
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    \f
+    :param role: The role to request (student, schooladministrator, etc...)
+    :type role: basestring
+    :param school: The school where to get the users
+    :type school: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List with all informations of all users with this role (as dict)
+    :rtype: list
     """
+
 
     if 'global' in role:
         return lr.get(f'/roles/{role}')
