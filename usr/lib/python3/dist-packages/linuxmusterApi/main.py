e6efeded6199994636431f23e714b06758c1e5af
kiarn <arnaud@kientz.me>
2024-06-18 09:34:04 +0200

    Update Swagger UI description.

--- usr/lib/python3/dist-packages/linuxmusterApi/main.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/main.py
@@ -8,8 +8,9 @@ import sys
 import base64
 import binascii
 from fastapi import FastAPI, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 
 config = {}
@@ -18,7 +19,33 @@ if os.path.isfile(config_path):
     with open(config_path, 'r') as config_file:
         config = yaml.load(config_file, Loader=yaml.SafeLoader)
 
-app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
+description = """
+
+This API provides access to a [linuxmuster.net](http://www.linuxmuster.net/)'s server management, like listing schoolclasses, users, starting an exam, etc ...
+
+### Security
+
+The endpoints are per role and user secured. Each request MUST provide a valid **JWT (JSON Web Token)** in the header (key `X-Api-Key`) to get the data.
+
+In order to get a valid JWT, you first need to send username and password via Basic auth at the endpoint [https://SERVER:8001/v1/auth](/v1/auth).
+
+### First request
+
+You are yet so far to launch your first request, just send a GET request with your JWT to [https://SERVER:8001/v1/schoolclasses](/v1/schoolclasses) and you will get a whole list of all schoolclasses on the server ! Have fun with it :)
+"""
+
+app = FastAPI(
+    title = "Linuxmuster.net API",
+    version="7.2.8",
+    description = description,
+    swagger_ui_parameters = {"tryItOutEnabled": True, "swagger_favicon_url": "/static/favicon.png"},
+    license_info={
+        "name": "GNU General Public License v3.0 only",
+        "url": "https://www.gnu.org/licenses/gpl-3.0.html"
+    },
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if config.get("cors", {}):
     app.add_middleware(
@@ -43,17 +70,6 @@ from routers_v1 import (
     users,
 )
 
-app.include_router(auth.router, prefix="/v1")
-app.include_router(exam.router, prefix="/v1")
-app.include_router(query.router, prefix="/v1")
-app.include_router(managementgroups.router, prefix="/v1")
-app.include_router(projects.router, prefix="/v1")
-app.include_router(schoolclasses.router, prefix="/v1")
-app.include_router(sessions.router, prefix="/v1")
-app.include_router(roles.router, prefix="/v1")
-app.include_router(teachers.router, prefix="/v1")
-app.include_router(users.router, prefix="/v1")
-
 @app.middleware("http")
 async def add_process_time_logging(request: Request, call_next):
     """
@@ -68,18 +84,32 @@ async def add_process_time_logging(request: Request, call_next):
 
 @app.get("/", response_class=HTMLResponse, tags=["Home"])
 def home():
-    """Default response at /"""
+    """
+    ### Default root response
+    """
 
     return """
 <html>
     <head>
-        <title>Linuxmuster-api</title>
+        <title>Linuxmuster.net API</title>
     </head>
     <body>
-        <h1>Empty response</h1>
+        <h1>Linuxmuster.net API</h1>
+        <p>Please refer to the <a href="docs/">documentation</a> to use this API</p>
     </body>
 </html>
     """
+
+app.include_router(auth.router, prefix="/v1")
+app.include_router(roles.router, prefix="/v1")
+app.include_router(users.router, prefix="/v1")
+app.include_router(teachers.router, prefix="/v1")
+app.include_router(query.router, prefix="/v1")
+app.include_router(schoolclasses.router, prefix="/v1")
+app.include_router(managementgroups.router, prefix="/v1")
+app.include_router(projects.router, prefix="/v1")
+app.include_router(sessions.router, prefix="/v1")
+app.include_router(exam.router, prefix="/v1")
 
 if __name__ == "__main__":
     secret = config.get('secret', None)
@@ -102,6 +132,7 @@ if __name__ == "__main__":
     config.setdefault('uvicorn', {})
     config['uvicorn'].setdefault('host', '0.0.0.0')
     config['uvicorn'].setdefault('port', 8001)
+
     # Using the same certificates as the Webui
     config['uvicorn'].setdefault('ssl_keyfile', '/etc/linuxmuster/api/lmnapi.pem')
     config['uvicorn'].setdefault('ssl_certfile', '/etc/linuxmuster/api/lmnapi.pem')
