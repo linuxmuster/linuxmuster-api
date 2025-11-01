f645652687b34e40bb67c1bbd68d3488a5e7844d
kiarn <arnaud@linuxmuster.net>
2025-11-01 17:54:49 +0100

    Centralize vars.

--- usr/lib/python3/dist-packages/linuxmusterApi/main.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/main.py
@@ -12,7 +12,7 @@ from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.utils import get_openapi
-
+from vars import *
 
 config = {}
 config_path = '/etc/linuxmuster/api/config.yml'
@@ -21,25 +21,10 @@ if os.path.isfile(config_path):
     with open(config_path, 'r') as config_file:
         config = yaml.load(config_file, Loader=yaml.SafeLoader)
 
-description = """
-
-This API provides access to a [linuxmuster.net](http://www.linuxmuster.net/)'s server management, like listing schoolclasses, users, starting an exam, etc ...
-
-### Security
-
-The endpoints are per role and user secured. Each request MUST provide a valid **JWT (JSON Web Token)** in the header (key `X-Api-Key`) to get the data.
-
-In order to get a valid JWT, you first need to send username and password via Basic auth at the endpoint [https://SERVER:8001/v1/auth](/v1/auth).
-
-### First request
-
-You are yet so far to launch your first request, just send a GET request with your JWT to [https://SERVER:8001/v1/schoolclasses](/v1/schoolclasses) and you will get a whole list of all schoolclasses on the server ! Have fun with it :)
-"""
-
 app = FastAPI(
-    title = "Linuxmuster.net API",
-    version="7.3.15",
-    description = description,
+    title = TITLE,
+    version=VERSION,
+    description = DESCRIPTION,
     swagger_ui_parameters = {"tryItOutEnabled": True, "swagger_favicon_url": "/static/favicon.png"},
     license_info={
         "name": "GNU General Public License v3.0 only",
@@ -96,17 +81,7 @@ def home():
     ### Default root response
     """
 
-    return """
-<html>
-    <head>
-        <title>Linuxmuster.net API</title>
-    </head>
-    <body>
-        <h1>Linuxmuster.net API</h1>
-        <p>Please refer to the <a href="docs/">documentation</a> to use this API</p>
-    </body>
-</html>
-    """
+    return HTML_HOME
 
 app.include_router(auth.router, prefix="/v1")
 app.include_router(schools.router, prefix="/v1")
@@ -129,10 +104,10 @@ def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
     openapi_schema = get_openapi(
-        title = "Linuxmuster.net API",
-        version = "7.3.13",
-        summary = "Linuxmuster.net API",
-        description = description,
+        title = TITLE,
+        version = VERSION,
+        summary = TITLE,
+        description = DESCRIPTION,
         routes=app.routes,
     )
     openapi_schema["components"]["securitySchemes"] = {
---
+++ usr/lib/python3/dist-packages/linuxmusterApi/vars.py
@@ -0,0 +1,35 @@
+VERSION = "7.3.15"
+TITLE = "Linuxmuster.net API"
+
+DESCRIPTION = """
+
+This API provides access to a [linuxmuster.net](http://www.linuxmuster.net/)'s server management, like listing schoolclasses, users, starting an exam, etc ...
+## Security
+
+### Firewall
+
+⚠️ In his default configuration, `linuxmuster-api` listen on port 8001. Since the API can provide sensitive information, it's **very important** to restrict the access to this port to only the clients or machines who need it.
+
+### Authentication
+
+The endpoints are per role and per user secured. 
+Each request MUST provide a valid **JWT (JSON Web Token)** in the header (key `X-Api-Key`) to get the data.
+
+An user can get a valid JWT token by sending username and password via Basic auth at the endpoint https://SERVER:8001/v1/auth.
+
+## First request
+
+You are yet so far to launch your first request, just send a GET request with your JWT to [https://SERVER:8001/v1/schoolclasses](/v1/schoolclasses) and you will get a whole list of all schoolclasses on the server ! Have fun with it :)
+"""
+
+HTML_HOME = f"""
+<html>
+    <head>
+        <title>Linuxmuster.net API {VERSION}</title>
+    </head>
+    <body>
+        <h1>Linuxmuster.net API {VERSION}</h1>
+        <p>Please refer to the <a href="docs/">documentation</a> to use this API</p>
+    </body>
+</html>
+    """
