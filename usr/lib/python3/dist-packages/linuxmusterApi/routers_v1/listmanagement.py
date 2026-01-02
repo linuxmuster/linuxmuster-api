437e2865ade35c99205a012cd3b900aa24e2b991
kiarn <arnaud@linuxmuster.net>
2026-01-02 17:11:09 +0100

    Post content of mgmt CSV file.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/listmanagement.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/listmanagement.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from security import RoleChecker, AuthenticatedUser
 from utils.checks import check_valid_mgmtlist_or_404
 from linuxmusterTools.lmnfile import LMNFile
+from .body_schemas import MgmtList
 
 
 router = APIRouter(
@@ -13,7 +14,7 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/{school}/{mgmtlist}", name="Get details of a specific management group")
+@router.get("/{school}/{mgmtlist}", name="Get the content of a specific management list")
 def get_management_list_content(school: str, mgmtlist: str, who: AuthenticatedUser = Depends(RoleChecker("GS"))):
     """
     ## Get the content of a management list (file like /etc/linuxmuster/sophomorix/default-school/teachers.csv).
@@ -26,7 +27,7 @@ def get_management_list_content(school: str, mgmtlist: str, who: AuthenticatedUs
 
     \f
     :param mgmtlist: A valid role (plural) like 'teachers'
-    :type group: basestring
+    :type mgmtlist: basestring
     :param who: User requesting the data, read from API Token
     :type who: AuthenticatedUser
     :return: Content of the csv file (list of dict, one dict per line in CSV)
@@ -41,3 +42,35 @@ def get_management_list_content(school: str, mgmtlist: str, who: AuthenticatedUs
             return list.read()
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error reading {path}: {str(e)}")
+
+@router.post("/{school}/{mgmtlist}", name="Write content of a specific management list")
+def post_management_list_content(school: str, mgmtlist: str, content: MgmtList, who: AuthenticatedUser = Depends(RoleChecker("GS"))):
+    """
+    ## Write the content of a management list (file like /etc/linuxmuster/sophomorix/default-school/teachers.csv).
+    The school must be extra given, because it's not possible to know which school a global-administrator would like
+    to request. This will overwrite the content of the CSV file, but LMNFile automatically makes a backup of the old
+    CSV file.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    \f
+    :param mgmtlist: A valid role (plural) like 'teachers'
+    :type mgmtlist: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :param content: Content of the CSV, see MgmtList attributes
+    :type content: MgmtList
+    :return: Content of the csv file (list of dict, one dict per line in CSV)
+    :rtype: list
+    """
+
+
+    path = check_valid_mgmtlist_or_404(mgmtlist, school)
+
+    try:
+        with LMNFile(path, 'w') as list:
+            return list.write(content.data)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error writing {path}: {str(e)}")
