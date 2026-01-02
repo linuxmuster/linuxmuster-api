fd7b6d52507858013aa92d3ad727126f591415ac
kiarn <arnaud@linuxmuster.net>
2026-01-02 19:28:33 +0100

    Apply mgmt list changes with sophomorix. EXPERIMENTAL.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/listmanagement.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/listmanagement.py
@@ -1,10 +1,13 @@
 import ldap
 import logging
+import tempfile
+import subprocess
 from fastapi import APIRouter, Depends, HTTPException
 
 from security import RoleChecker, AuthenticatedUser
 from utils.checks import check_valid_mgmtlist_or_404
 from linuxmusterTools.lmnfile import LMNFile
+from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 from utils.sophomorix import lmn_getSophomorixValue
 from .body_schemas import MgmtList
 
@@ -27,6 +30,8 @@ def get_management_list_content(school: str, mgmtlist: str, who: AuthenticatedUs
     - school-administrators
 
     \f
+    :param school: A valid school where to get the content
+    :type school: basestring
     :param mgmtlist: A valid role (plural) like 'teachers'
     :type mgmtlist: basestring
     :param who: User requesting the data, read from API Token
@@ -57,6 +62,8 @@ def post_management_list_content(school: str, mgmtlist: str, content: MgmtList, 
     - school-administrators
 
     \f
+    :param school: A valid school where to post the content
+    :type school: basestring
     :param mgmtlist: A valid role (plural) like 'teachers'
     :type mgmtlist: basestring
     :param who: User requesting the data, read from API Token
@@ -105,3 +112,65 @@ def do_sophomorix_check(who: AuthenticatedUser = Depends(RoleChecker("GS"))):
                 if user_update in results["CHECK_RESULT"]["KILL"]:
                     del results["CHECK_RESULT"]["UPDATE"][user_update]
     return results
+
+@router.get("/sophomorix-apply", name="Run sophomorix-add, sophomorix-update, sophomorix-kill or all together.")
+def do_sophomorix_apply(
+        school: str,
+        add: bool = False,
+        update: bool = False,
+        kill: bool = False,
+        who: AuthenticatedUser = Depends(RoleChecker("GS"))):
+    """
+    ## Run sophomorix-add, sophomorix-update, sophomorix-kill or all together.
+    TODO: Actually in this form, the response could be a way too long. It would be better to launch a process and to
+    follow this process with other requests.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    ### This endpoint uses Sophomorix.
+
+    \f
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :param school: A valid school where to apply the changes
+    :type school: basestring
+    :param add: Bool to launch sophomorix-add or not
+    :param update: Bool to launch sophomorix-update or not
+    :param kill: Bool to launch sophomorix-kill or not
+    :return: Output of sophomorix-check
+    :rtype: basestring
+    """
+
+
+    if school not in lr.getval('/schools', 'ou'):
+        raise HTTPException(status_code=404, detail=f"{school} is not a valid school")
+
+    if who.school != "global" and who.school != school:
+        raise HTTPException(status_code=403, detail=f"Forbidden")
+
+    _, path = tempfile.mkstemp(prefix=f'{school}.', suffix='.sophomorix.log')
+
+    script = ''
+
+    if add:
+        script += f'sophomorix-add --school {school} >> {path};'
+
+    if update:
+        script += f'sophomorix-update --school {school} >> {path};'
+
+    if kill:
+        script += f'sophomorix-kill --school {school} >> {path};'
+
+    try:
+        subprocess.check_call(script, shell=True, env={'LC_ALL': 'C'})
+        with open(path, 'r') as f:
+            log = f.readlines()
+
+        # Remove log file ?
+
+        return log
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error applying changes with sophomorix: {str(e)}")
