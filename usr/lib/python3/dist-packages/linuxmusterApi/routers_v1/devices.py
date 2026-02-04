41b6677ba87bc491861f8f42d455fba03b8c563f
kiarn <arnaud@linuxmuster.net>
2026-02-04 12:19:03 +0100

    Writes devices.csv per POST.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/devices.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/devices.py
@@ -5,7 +5,7 @@ from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 from linuxmusterTools.lmnfile import LMNFile
 from utils.checks import get_printer_or_404
 from utils.sophomorix import lmn_getSophomorixValue
-from .body_schemas import Printer
+from .body_schemas import MgmtList
 
 
 router = APIRouter(
@@ -59,3 +59,40 @@ def get_all_devices(school: str, who: AuthenticatedUser = Depends(RoleChecker("G
         device['status'] = status
 
     return devices_data
+
+@router.post("/{school}/", name="Write content of devices.csv")
+def post_management_list_content(school: str, content: MgmtList, who: AuthenticatedUser = Depends(RoleChecker("GS"))):
+    """
+    ## Write the content of devices list (file like /etc/linuxmuster/sophomorix/default-school/devices.csv).
+    The school must be extra given, because it's not possible to know which school a global-administrator would like
+    to request. This will overwrite the content of the CSV file, but LMNFile automatically makes a backup of the old
+    CSV file.
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    \f
+    :param school: A valid school where to post the content
+    :type school: basestring
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :param content: Content of the CSV, see MgmtList attributes
+    :type content: MgmtList
+    :return: Content of the csv file (list of dict, one dict per line in CSV)
+    :rtype: list
+    """
+
+
+    if school != 'default-school':
+        prefix = f'{school}.'
+    else:
+        prefix = ''
+
+    path = f"/etc/linuxmuster/sophomorix/{school}/{prefix}devices.csv"
+
+    try:
+        with LMNFile(path, 'w') as list:
+            return list.write(content.data)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error writing {path}: {str(e)}")
