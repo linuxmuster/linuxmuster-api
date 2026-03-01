807a9398d7b1caf06e87d80f6a3b3f983a5c6301
kiarn <arnaud@linuxmuster.net>
2026-03-01 14:43:56 +0100

    Patch some attributes of a specific device.

--- usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/devices.py
+++ usr/lib/python3/dist-packages/linuxmusterApi/routers_v1/devices.py
@@ -3,11 +3,11 @@ import subprocess
 from fastapi import APIRouter, Depends, HTTPException
 
 from security import RoleChecker, AuthenticatedUser
-from linuxmusterTools.ldapconnector import LMNLdapReader as lr
+from linuxmusterTools.ldapconnector import LMNLdapReader as lr, LMNDevice
 from linuxmusterTools.lmnfile import LMNFile
 from utils.checks import get_printer_or_404
 from utils.sophomorix import lmn_getSophomorixValue
-from .body_schemas import MgmtList
+from .body_schemas import MgmtList, Device
 from utils.checks import check_valid_school_or_404
 
 router = APIRouter(
@@ -96,6 +96,39 @@ def get_device_details(device: str, who: AuthenticatedUser = Depends(RoleChecker
         return devices_data
 
     raise HTTPException(status_code=404, detail=f"Device {device} not found")
+
+@router.patch("/{device}", name="Update some attributes of a specific device")
+def modify_device(device: str, device_details: Device, who: AuthenticatedUser = Depends(RoleChecker("GS"))):
+    """
+    ## Update some attributes of a specific device
+
+    *device_details* are the attributes of the device, like *unicodePwd*,
+
+    ### Access
+    - global-administrators
+    - school-administrators
+
+    \f
+    :param device: cn of the device to update
+    :type device: basestring
+    :param device_details: Parameter of the device, see Device attributes
+    :type device_details: Device
+    :param who: User requesting the data, read from API Token
+    :type who: AuthenticatedUser
+    :return: List of all devices details (dict)
+    :rtype: list
+    """
+
+
+    # School specific request. For global-admins, it will search in all devices from all schools
+    device_writer = LMNDevice(device, school=who.school)
+
+    if not device_writer.getattr('sAMAccountName'):
+       raise HTTPException(status_code=404, detail=f"Device {device} not found.")
+
+    device_writer.setattr(data=device_details.model_dump())
+
+    return device_writer.data
 
 @router.post("/list/{school}", name="Write content of devices.csv")
 def post_management_list_content(school: str, content: MgmtList, who: AuthenticatedUser = Depends(RoleChecker("GS"))):
